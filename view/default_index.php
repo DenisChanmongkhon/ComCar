17f775bbc13b8c9425cf6febf798a9b964df725e
denis.chanmongkhon <denis.chanmongkhon@iet-gibb.ch>
2018-04-23 13:11:27 +0200

    added buttons for more information and add comparison

--- view/default_index.php
+++ view/default_index.php
@@ -3,7 +3,21 @@
                     <div class="panel-body">
                         <h1> #carname</h1>
                         <img src="<?php echo $entries->picture ?>" alt="image" >
-
+                        <button type="button" class="showmore" data-toggle="" data-target="#navbar" aria-expanded="false" aria-controls="addcar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="panel-body">
+                        <h4>More Details</h4>
                     </div>
                 </div>
+    <button type="button" class="addcomparison" data-toggle="" data-target="#navbar" aria-expanded="false" aria-controls="addcar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
 </article>
--- view/header.php
+++ view/header.php
@@ -13,6 +13,7 @@
 
     <!-- Custom styles for this template -->
       <link href="https://fonts.googleapis.com/css?family=Raleway:300" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
