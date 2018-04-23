2ca1b8797e4be6c9bfb30d6859f344778bb36eed
Xhenis <legitkami@gmail.com>
2018-04-23 13:14:56 +0200

    fancybox

--- view/default_index.php
+++ view/default_index.php
@@ -1,7 +1,7 @@
 <article class="hreview open special">
                 <div class="panel panel-default">
                     <div class="panel-body">
-
+                        <a data-fancybox="gallery" href="data/images">
                         <h1> #carname</h1>
                         <img src="<?php echo $entries->picture ?>" alt="image" >
 
