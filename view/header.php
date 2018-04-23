d97e9fec88b9f750b1e06de89d95734c37933d15
denis.chanmongkhon <denis.chanmongkhon@iet-gibb.ch>
2018-04-23 10:07:49 +0200

    übergabe xhenis

--- view/header.php
+++ view/header.php
@@ -19,8 +19,8 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+  <body><a class="navbar-brand" href="/">ComCar</a>
+    <!--<nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -29,18 +29,18 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/">Bbc MVC</a>
+          <a class="navbar-brand" href="/">ComCar</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li><a href="/">Home</a></li>
-            <li><a href="/user">Benutzer</a></li>
-            <li><a href="/user/create">Benutzer erstellen</a></li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div><!--/.nav-collapse
       </div>
-    </nav>
-
-    <div class="container">
+    </nav>-->
+  <div class="container">
+      <div class="navbar-header">
+      </div>
+  </div>
 
     <h1><?= $heading ?></h1>
