4c71d6e4f7aeb49ac80b3f33a6b476a4eda38c35
Xhenis <legitkami@gmail.com>
2018-04-23 13:08:16 +0200

    Added Fancybox

--- view/default_index.php
+++ view/default_index.php
@@ -1,6 +1,7 @@
 <article class="hreview open special">
                 <div class="panel panel-default">
                     <div class="panel-body">
+
                         <h1> #carname</h1>
                         <img src="<?php echo $entries->picture ?>" alt="image" >
 
