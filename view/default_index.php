187b431df6a426c518eae6cc880921695606c7f5
denis.chanmongkhon <denis.chanmongkhon@iet-gibb.ch>
2018-04-26 20:07:42 +0200

    trying to make that fucking button work with the cloning

--- view/default_index.php
+++ view/default_index.php
@@ -1,42 +1,53 @@
-
-
 <?php
 if (isset($_POST['submit'])) {
-    $selectedCar =$car->brandname;
+    $selectedBrand =$car->brandname;
+    $selectedModel =$car->model;
+}
+else{
+    $selectedBrand = 'Audi';
+    $selectedModel = 'RS7';
 }
 ?>
-                <div class="container">
-                    <div class="panel panel-default">
-                        <form method="POST" id="form" action="/default/doShow">
-                            <div class="panel-top">
+<script type="text/javascript" src="js/script.js"></script>
+<script>
+    $(document).ready(function(e) {
+
+    })
+
+
+</script>
+<div class="container" id="completeForm">
+     <div class="panel panel-container" id="entry1">
+           <form method="POST" id="form" action="/default/doShow">
+               <div class="panel-full">
+                <div class="panel-top">
                 <select name="brandname">
-                    <option <?php if($selectedCar == 'Audi'){echo("selected");}?> value="Audi">Audi</option>
-                    <option <?php if($selectedCar == 'Lamborghini'){echo("selected");}?> value="Lamborghini">Lamborghini</option>
-                    <option <?php if($selectedCar == 'Mercedes'){echo("selected");}?> value="Mercedes">Mercedes</option>
-                    <option <?php if($selectedCar == 'Porsche'){echo("selected");}?> value="Porsche">Porsche</option>
-                    <option <?php if($selectedCar == 'Tesla'){echo("selected");}?> value="Tesla">Tesla</option>
+                    <option <?php if($selectedBrand == 'Audi'){echo("selected");}?> value="Audi">Audi</option>
+                    <option <?php if($selectedBrand == 'Lamborghini'){echo("selected");}?> value="Lamborghini">Lamborghini</option>
+                    <option <?php if($selectedBrand == 'Mercedes'){echo("selected");}?> value="Mercedes">Mercedes</option>
+                    <option <?php if($selectedBrand == 'Porsche'){echo("selected");}?> value="Porsche">Porsche</option>
+                    <option <?php if($selectedBrand == 'Tesla'){echo("selected");}?> value="Tesla">Tesla</option>
                 </select>
                 <select name="model">
-                    <option value="Huracan">Huracan</option>
-                    <option value="Panamera">Panamera</option>
-                    <option value="RS7">RS7</option>
-                    <option value="Model X">Model X</option>
-                    <option value="AMG GT">AMG GT</option>
+                    <option <?php if($selectedModel == 'Huracan'){echo("selected");}?>value="Huracan">Huracan</option>
+                    <option <?php if($selectedModel == 'Panamera'){echo("selected");}?> value="Panamera">Panamera</option>
+                    <option <?php if($selectedModel == 'RS7'){echo("selected");}?> value="RS7">RS7</option>
+                    <option <?php if($selectedModel == 'Model X'){echo("selected");}?> value="Model ">Model X</option>
+                    <option <?php if($selectedModel == 'AMG GT'){echo("selected");}?> value="AMG GT">AMG GT</option>
                 </select><input type="submit" name="submit" class="submit" value="Select"/>
 
 
             <?php
             if (isset($_POST['submit'])) {
                 echo '
-                    
-                        <a data-fancybox="gallery" href=" '. $car->path .' ">
+                <a data-fancybox="gallery" href=" '. $car->path .' ">
 
                     
                         <a id="single_image" href=" '. $car->path .' ">
                             <img src=" '. $car->path .' " alt="" style="width:370px" />
                         </a>
-            </div>
-                    <div class="panel-bottom">
+                </div>
+                    <div class="panel-body">
                     <table class="table">
                              <tbody>
                                  <tr>
@@ -96,13 +107,12 @@ if (isset($_POST['submit'])) {
             }
             ?>
             </div>
+            </div>
         </form>
     </div>
-    <form method="GET" id="form" action="/default/addForm">
-        <div class="addform">
-            <button class="addcomparison" >+</button>
-        </div>
-    </form>
+
+            <button class="addcomparison" id="addnewForm">+</button>
+
 
 
 </div>
--- view/header.php
+++ view/header.php
@@ -19,10 +19,12 @@
       <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
       <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-      <script src="/js/script.js"></script>
+      <script type="text/javascript" src="/js/script.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
